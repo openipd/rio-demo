2eeb3f09ee6d7b53b3a4ded05eeb71bc34d14bfc
Andreas Binder <abi@s-pe.eu.com>
2019-09-05 19:46:44 +0200

    update

--- main.go
+++ main.go
@@ -10,7 +10,7 @@ import (
 func handler(w http.ResponseWriter, r *http.Request) {
 	// Arbitrary sleep so that we can demonstrate autoscaler
 	time.Sleep(100 * time.Millisecond)
-	fmt.Fprintln(w, "Hi there, I'm running in Kubernetes working for BMG and ABI")
+	fmt.Fprintln(w, "Hi there, I'm running in Kubernetes working for SPE")
 }
 
 func main() {
