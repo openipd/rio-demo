cea318c9a0431ac29be1c7a2c660f4046bd442ec
DAB <openabi@gmail.com>
2021-02-25 18:00:42 +0100

    update

--- main.go
+++ main.go
@@ -17,7 +17,7 @@ func handler(w http.ResponseWriter, r *http.Request) {
 		panic(err)
 	}
 
-	fmt.Fprintln(w, "Hi newest friends, I'm running in Kubernetes Pod:", name)
+	fmt.Fprintln(w, "Hi, I'm running in Kubernetes Pod:", name)
 }
 
 func main() {
